5989cc49d48cf9dfdb6f0c008283a9fe3f0a947b
Brian Shacklett <brian.shacklett@globaloptions.com>
2015-01-15 13:47:36 -0500

    Refactored Add and Remove-Member cmdlets

--- PSIdentityManagement.psm1
+++ PSIdentityManagement.psm1
@@ -79,41 +79,7 @@ function Add-GroupMember {
 
     Process
     {
-        $group = Get-SecurityObject -Identity $Identity
-        $groupBase= $group.PSBase
-        
-        $Members | % {
-            
-            $member = Get-SecurityObject -Identity $_
-            $memberBase= $member.PSBase
-
-
-            Write-Debug ('{0}: Group: {1}, Member: {2}' -f $MyInvocation.MyCommand, $group.PSBase.Path, $member.PSBase.Path)
-
-            If ($PSCmdlet.ShouldProcess(('Member "{0}" of group "{1}"' -f $memberBase.Path, $groupBase.Path)))
-            {
-                # Make sure that $group is actually a group.
-                If ($group.groupType -eq "")
-                {
-                    Throw ('The Security object "{0}" is not a group and cannot have members added to it.' -f $groupBase.Path)
-                }
-
-                # Wrap the action and status message in a try/catch so that it
-                # doesn't give spurious status outputs.
-                Try 
-                {
-                    $group.PSBase.Invoke("Add",$member.PSBase.Path)
-
-                    Write-Verbose ('The member {0} was successfully added to the group {1}.' -f $member.PSBase.Path, $group.PSBase.Path)
-                }
-                # I'm not sure how to handle this, yet, so just rethrow the
-                # full exception.
-                Catch
-                {
-                    Throw $_
-                }
-            }
-        }
+        Set-GroupMembership -Identity $Identity -Members $Members -Disposition 'Add'
     }
 }
 
@@ -143,23 +109,84 @@ function Remove-GroupMember {
 
     Process
     {
+        Set-GroupMembership -Identity $Identity -Members $Members -Disposition 'Remove'
+    }
+}
+
+
+
+
+
+
+function Set-GroupMembership {
+    <##>
+
+    [CmdletBinding(SupportsShouldProcess=$True)]
+    param
+    (
+        # Accept groups in pipeline input
+        [parameter(Mandatory=$True,ValueFromPipeline=$True)]
+        [string]
+        $Identity,
+
+        [parameter(Mandatory=$True)]
+        [string]
+        $Members,
+
+        [parameter(Mandatory=$True)]
+        [ValidateSet("Add","Remove")]
+        [string]$Disposition
+    )
+
+    Begin
+    {
+        IF ( $Disposition -eq "Add" )
+        {
+            [string]$TargetFormat = 'Member "{0}" of group "{1}"'
+            [string]$NotAGroupExceptionFormat = 'The Security object "{0}" is not a group and cannot have members added to it.'
+            [string]$ResultFormat = 'The member {0} was successfully added to group {1}'
+        }
+        Else
+        {
+            [string]$TargetFormat = 'Member "{0}" of group "{1}"'
+            [string]$NotAGroupExceptionFormat = 'The Security object "{0}" is not a group and cannot have members removed from it.'
+            [string]$ResultFormat = 'The member {0} was successfully removed from the group {1}'
+        }
+    }
+
+    Process
+    {
         $group = Get-SecurityObject -Identity $Identity
         $groupBase = $group.PSBase
         
         $Members | % {
             
-            $member = Get-SecurityObject -Identity $_
-            $memberBase = $member.PSBase
-
-            If ($PSCmdlet.ShouldProcess(('Member "{0}" of group "{1}"' -f $memberBase.Path, $groupBase.Path)))
+            Try
             {
+                $member = Get-SecurityObject -Identity $_
+                $memberBase = $member.PSBase
+            }
+            Catch
+            {
+                Throw $_
+            }
+
+            If ($PSCmdlet.ShouldProcess(($TargetFormat -f $memberBase.Path, $groupBase.Path)))
+            {
+                # Make sure that $group is actually a group.
+                If ($group.groupType -eq "")
+                {
+                    Throw ($NotAGroupExceptionFormat -f $groupBase.Path)
+                }
+
                 # Wrap the action and status message in a try/catch so that it
                 # doesn't give spurious status outputs.
                 Try
                 {
-                    $group.PSBase.Invoke("Remove",$member.PSBase.Path)
+                    $group.PSBase.Invoke($Disposition,$member.PSBase.Path)
 
-                    Write-Verbose ('The member {0} was successfully removed from the group {1}' -f $memberBase.Path, $groupBase.Path)
+                    # Write the result to the verbose stream.
+                    Write-Verbose ($ResultFormat -f $memberBase.Path, $groupBase.Path)
                 }
                 # I'm not sure how to handle this, yet, so just rethrow the
                 # full exception.
@@ -171,3 +198,4 @@ function Remove-GroupMember {
         }
     }
 }
+
